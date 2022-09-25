81b4c4ed12bc1665c106ed70d7a977b37b0ee11a
Camila Vertiz Asmat <N00235579@upn.pe>
2022-09-25 17:11:01 -0500

    validacion login ok

--- ProyectoFinal_DISARQ/MenuAdmin.Designer.cs
+++ ProyectoFinal_DISARQ/MenuAdmin.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureLogout = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureLogout
+            // 
+            this.pictureLogout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureLogout.Image = global::CapaPresentacion.Properties.Resources.logout1;
+            this.pictureLogout.Location = new System.Drawing.Point(83, 102);
+            this.pictureLogout.Name = "pictureLogout";
+            this.pictureLogout.Size = new System.Drawing.Size(30, 30);
+            this.pictureLogout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureLogout.TabIndex = 0;
+            this.pictureLogout.TabStop = false;
+            this.pictureLogout.Click += new System.EventHandler(this.pictureLogout_Click);
+            // 
+            // MenuAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureLogout);
+            this.Name = "MenuAdmin";
             this.Text = "MenuAdmin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogout)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox pictureLogout;
     }
 }
--- ProyectoFinal_DISARQ/Properties/Resources.Designer.cs
+++ ProyectoFinal_DISARQ/Properties/Resources.Designer.cs
@@ -73,6 +73,26 @@ namespace CapaPresentacion.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap logout {
+            get {
+                object obj = ResourceManager.GetObject("logout", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logout1 {
+            get {
+                object obj = ResourceManager.GetObject("logout1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Rokito {
             get {
                 object obj = ResourceManager.GetObject("Rokito", resourceCulture);
