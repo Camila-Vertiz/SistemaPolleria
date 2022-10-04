475a334df982bba45bd0f99eba97365fbdbd4b46
Edson Contreras <edson.contreras113@gmail.com>
2022-10-04 10:21:28 -0500

    mantenedor Mesas completo

--- ProyectoFinal_DISARQ/FormMantenedorMesa.Designer.cs
+++ ProyectoFinal_DISARQ/FormMantenedorMesa.Designer.cs
@@ -40,6 +40,9 @@
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtIdMesa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEditar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMesa)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,35 +53,36 @@
             this.dgvMesa.Name = "dgvMesa";
             this.dgvMesa.RowHeadersWidth = 51;
             this.dgvMesa.RowTemplate.Height = 29;
-            this.dgvMesa.Size = new System.Drawing.Size(638, 219);
+            this.dgvMesa.Size = new System.Drawing.Size(594, 219);
             this.dgvMesa.TabIndex = 27;
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(382, 85);
+            this.btnBuscar.Location = new System.Drawing.Point(375, 75);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(80, 29);
             this.btnBuscar.TabIndex = 26;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // txtUbicacion
             // 
-            this.txtUbicacion.Location = new System.Drawing.Point(237, 177);
+            this.txtUbicacion.Location = new System.Drawing.Point(236, 208);
             this.txtUbicacion.Name = "txtUbicacion";
             this.txtUbicacion.Size = new System.Drawing.Size(125, 27);
             this.txtUbicacion.TabIndex = 24;
             // 
             // txtCapacidad
             // 
-            this.txtCapacidad.Location = new System.Drawing.Point(237, 132);
+            this.txtCapacidad.Location = new System.Drawing.Point(236, 163);
             this.txtCapacidad.Name = "txtCapacidad";
             this.txtCapacidad.Size = new System.Drawing.Size(125, 27);
             this.txtCapacidad.TabIndex = 23;
             // 
             // txtNumeroMesa
             // 
-            this.txtNumeroMesa.Location = new System.Drawing.Point(237, 89);
+            this.txtNumeroMesa.Location = new System.Drawing.Point(236, 119);
             this.txtNumeroMesa.Name = "txtNumeroMesa";
             this.txtNumeroMesa.Size = new System.Drawing.Size(125, 27);
             this.txtNumeroMesa.TabIndex = 22;
@@ -96,7 +100,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(104, 177);
+            this.label3.Location = new System.Drawing.Point(103, 208);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 20);
             this.label3.TabIndex = 19;
@@ -105,7 +109,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(104, 132);
+            this.label2.Location = new System.Drawing.Point(103, 163);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(83, 20);
             this.label2.TabIndex = 18;
@@ -114,7 +118,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(104, 89);
+            this.label1.Location = new System.Drawing.Point(103, 119);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 20);
             this.label1.TabIndex = 17;
@@ -128,6 +132,7 @@
             this.btnEliminar.TabIndex = 16;
             this.btnEliminar.Text = "Eliminar Mesa";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // btnModificar
             // 
@@ -148,12 +153,41 @@
             this.btnRegistrar.UseVisualStyleBackColor = true;
             this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
+            // txtIdMesa
+            // 
+            this.txtIdMesa.Location = new System.Drawing.Point(235, 75);
+            this.txtIdMesa.Name = "txtIdMesa";
+            this.txtIdMesa.Size = new System.Drawing.Size(125, 27);
+            this.txtIdMesa.TabIndex = 29;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Id de Mesa";
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(711, 81);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(161, 29);
+            this.btnEditar.TabIndex = 30;
+            this.btnEditar.Text = "Editar Mesa";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
             // FormMantenedorMesa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gold;
-            this.ClientSize = new System.Drawing.Size(800, 502);
+            this.ClientSize = new System.Drawing.Size(927, 502);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.txtIdMesa);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.dgvMesa);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.txtUbicacion);
@@ -188,5 +222,8 @@
         private Button btnEliminar;
         private Button btnModificar;
         private Button btnRegistrar;
+        private TextBox txtIdMesa;
+        private Label label4;
+        private Button btnEditar;
     }
 }
--- ProyectoFinal_DISARQ/FormMantenedorMesa.cs
+++ ProyectoFinal_DISARQ/FormMantenedorMesa.cs
@@ -43,5 +43,64 @@ namespace CapaPresentacion
             }
             listaMesas();
         }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            txtIdMesa.Focus();
+            int idMesas = Convert.ToInt32(txtIdMesa.Text);
+            entMesas Cal = logMesas.Instancia.BuscaMesas(idMesas);
+            if (Cal != null)
+            {
+
+                txtIdMesa.Text = Convert.ToString(Cal.id_mesa);
+                txtNumeroMesa.Text = Cal.numero_mesa;
+                txtCapacidad.Text = Convert.ToString(Cal.capacidad_mesa);
+                txtUbicacion.Text = Cal.ubicacion_mesa;
+                MessageBox.Show("Registro Encontrado");
+            }
+            else
+                MessageBox.Show("El Calzado no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                entMesas m = new entMesas();
+                m.id_mesa = int.Parse(txtIdMesa.Text.Trim());
+                m.numero_mesa = txtNumeroMesa.Text.Trim();
+                m.capacidad_mesa = int.Parse(txtCapacidad.Text.Trim());
+                m.ubicacion_mesa = txtUbicacion.Text.Trim();
+                logMesas.Instancia.EditaMesas(m);
+                MessageBox.Show("Registro Modificado");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+           
+            listaMesas();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    entMesas m = new entMesas();
+
+                    m.id_mesa = int.Parse(txtIdMesa.Text.Trim());
+                    logMesas.Instancia.EliminaMesas(m);
+                     MessageBox.Show("Registro eliminado con exito!" );
+            }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error.." + ex);
+                }
+            listaMesas();
+            
+        }
     }
+    
 }
